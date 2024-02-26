b0817df6ebf3d4dfe6e80ea3da0457a04519f86f
MartijnVanLeer <100680324+MartijnVanLeer@users.noreply.github.com>
2024-02-26 09:46:02 +0100

    uitlezer fix

--- Scripts/Runner.py
+++ Scripts/Runner.py
@@ -12,7 +12,8 @@ Layer = snakemake.params.simlayer
 
 
 idx = pd.read_csv(os.path.join('..','Results',f'{model_name}',f'idx_SS_{model_name}.csv'))
-ObsHeads =pd.read_csv(os.path.join('..','Results',f'{model_name}',f'ObsHead_{model_name}.csv'))
+ObsHeads =pd.read_csv(os.path.join('..','Results',f'{model_name}',f'ObsHead_{model_name}.csv'), index_col = 'Time')
+pd. to_datetime(ObsHeads.index)
 ObsWells = pd.read_csv(os.path.join('..','Results',f'{model_name}',f'ObsForCalibration_{model_name}_SS.csv'))
 layno = idx[idx.SensLayers == Layer].idx.values[0]
 
