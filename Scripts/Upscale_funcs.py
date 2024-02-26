b8a530467ac76e791cb5f6a2f40860a6c42919bd
MartijnVanLeer <100680324+MartijnVanLeer@users.noreply.github.com>
2024-02-26 23:19:30 +0100

    anisotropie

--- Scripts/Upscale_funcs.py
+++ Scripts/Upscale_funcs.py
@@ -25,7 +25,7 @@ def setup_mf(Lx,Ly,Lz,dx,dy,dz,z, mds,ws):
              chd_spd.append( [(int(Lz/dz)-1,row,col),Lz-dz])
     chd = flopy.mf6.ModflowGwfchd(gwf, stress_period_data=chd_spd, save_flows=True)
     oc = flopy.mf6.ModflowGwfoc(gwf,head_filerecord='Upscaler' + ".hds", budget_filerecord="{}.cbc".format('Upscaler'), saverecord=[('BUDGET', 'ALL'), ('HEAD', 'ALL')])
-    npf = flopy.mf6.ModflowGwfnpf(gwf, k = 10, k33 = 1,  k33overk = True, save_specific_discharge=True)
+    npf = flopy.mf6.ModflowGwfnpf(gwf, k = 10, k33 = 1,  k33overk = False, save_specific_discharge=True)
     sim.write_simulation(silent = True)
     return sim
 
@@ -44,11 +44,12 @@ def Run_MF_WholeField(Kfields,Lx,Ly,Lz,dx,dy,dz, mds, ws):
 def run_mf(sim, Kfield,mds, ws):
     gwf = sim.get_model()
     npf = gwf.get_package('NPF')
+    npf.k33 = np.rot90(Kfield, k=1, axes = (0,2))
     npf.k = np.rot90(Kfield, k=1, axes = (0,2))
     npf.write()
     sim.run_simulation(silent = True)
     cbb = flopy.utils.CellBudgetFile(os.path.join(ws, f"{gwf.name}.cbc"))
-    qs = cbb.get_data(text='DATA-SPDIS')
+    qs = cbb.get_data(text='DATA-SPDIS')[0]
     qx, qy, qz = flopy.utils.postprocessing.get_specific_discharge(qs, gwf)
     K = abs(qz[:, :,:].mean())
     return K
