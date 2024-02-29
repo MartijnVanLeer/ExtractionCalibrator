1f8328a56ee026cd040a2c2d8fbcefe3f084427c
MartijnVanLeer <100680324+MartijnVanLeer@users.noreply.github.com>
2024-02-29 16:43:41 +0100

    foutje bedankt

--- Scripts/Heterogeniteit.py
+++ Scripts/Heterogeniteit.py
@@ -91,9 +91,7 @@ class boringen():
             mid = ds.sel(icell2d = xy_to_icell2d((df['x'].values[0],df['y'].values[0]), ds)).Layermid.values
             minrange = mid - 0.5 * condrange
             maxrange = mid + 0.5 * condrange
-            print(condrange)
-            print (mid)
-            df = df[df['z'] >= minrange & df['z'] < maxrange)]
+            df = df[df['z'].between(minrange,maxrange)]
             df['z'] = np.arange(len(df))
             self.selection[boringnr] = df
     
