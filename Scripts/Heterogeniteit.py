658a1a5534eff364db99899bbbcf9ff66ccc2aef
MartijnVanLeer <100680324+MartijnVanLeer@users.noreply.github.com>
2024-02-29 16:02:04 +0100

    :

--- Scripts/Heterogeniteit.py
+++ Scripts/Heterogeniteit.py
@@ -50,8 +50,8 @@ class boringen():
                     row['quality'] = md.loc['Kwaliteitcode beschrijving lithologie', 'ALGEMENE GEGEVENS BORING']
                     row['boringnr'] = ix
                     result.append(row.copy())
-            if (md.loc['X-coordinaat (m)'].values[0] >= ds.extent[0] & md.loc['X-coordinaat (m)'].values[0] <=  ds.extent[1])
-                if (md.loc['X-coordinaat (m)'].values[0] >= ds.extent[2] & md.loc['X-coordinaat (m)'].values[0] <=  ds.extent[3])
+            if (md.loc['X-coordinaat (m)'].values[0] >= ds.extent[0] & md.loc['X-coordinaat (m)'].values[0] <=  ds.extent[1]):
+                if (md.loc['X-coordinaat (m)'].values[0] >= ds.extent[2] & md.loc['X-coordinaat (m)'].values[0] <=  ds.extent[3]):
                     self.boreholes[name] = pd.DataFrame(result)
         self.metadata = pd.concat(metadata, axis = 1)
         self.metadata.columns = self.metadata.iloc[0]
