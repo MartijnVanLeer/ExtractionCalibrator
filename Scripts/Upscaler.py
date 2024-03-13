b4bff41cd1f2e87749791a5da33525cc708859c2
MartijnVanLeer <100680324+MartijnVanLeer@users.noreply.github.com>
2024-03-13 13:23:27 +0100

    simpler

--- Scripts/Upscaler.py
+++ Scripts/Upscaler.py
@@ -36,7 +36,7 @@ result = xr.Dataset(data_vars=dict( k = (['sim', 'icell2d'], np.zeros((ens_no, l
 def add_cellid(Kfields,ds, layer):
     cellids = [] 
     for index, row in Kfields.iterrows():
-        layer, cellid = xyz_to_cid((row.x,row.y,ds.sel(layer = layer).botm.values.mean()), ds)
+        layer, cellid = xyz_to_cid((row.x,row.y,-50, ds)
         cellids.append(cellid)
     Kfields['cellid'] = cellids
     return Kfields
