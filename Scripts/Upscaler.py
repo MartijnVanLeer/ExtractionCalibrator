fdf225542e2cbfabe783691a3ea7f10e182af49d
MartijnVanLeer <100680324+MartijnVanLeer@users.noreply.github.com>
2024-03-14 12:30:42 +0100

    nantest fix

--- Scripts/Upscaler.py
+++ Scripts/Upscaler.py
@@ -43,8 +43,8 @@ df.set_index(['x', 'y', 'z'], inplace = True)
 test = df[df.cellid == 12]
 testk= xr.Dataset.from_dataframe(test)
 testk.to_netcdf('test.nc')
-print(np.count_nonzero(np.isnan(testk)))
-df.set_index(['x', 'y', 'z'], inplace = True)
+print(np.count_nonzero(np.isnan(testk.values)))
+
 #run modflow for modelcell for all realizations
 for cellid in tqdm(ids):
     cell = df[df.cellid == cellid]
