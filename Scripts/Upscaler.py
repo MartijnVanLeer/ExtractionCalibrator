9729f2d72f67f453acda3d341cb5dec1312237bb
MartijnVanLeer <100680324+MartijnVanLeer@users.noreply.github.com>
2024-03-15 13:27:11 +0100

    Upscaler werkend!

--- Scripts/Upscaler.py
+++ Scripts/Upscaler.py
@@ -41,12 +41,6 @@ result = xr.Dataset(data_vars=dict( k = (['sim', 'icell2d'], np.zeros((ens_no, l
 
 df.set_index(['x', 'y', 'z'], inplace = True)
 
-test = df[df.cellid == 12]
-testk= xr.Dataset.from_dataframe(test)
-testk.to_netcdf('test.nc')
-print(np.count_nonzero(np.isnan(testk["K_1"].values)))
-
-
 #run modflow for modelcell for all realizations
 for cellid in tqdm(ids):
     cell = df[df.cellid == cellid]
