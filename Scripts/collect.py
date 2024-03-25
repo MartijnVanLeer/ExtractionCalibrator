3816c8c646c7e4a2975dda6b4fe1b4099170e9bd
MartijnVanLeer <100680324+MartijnVanLeer@users.noreply.github.com>
2024-03-25 15:45:07 +0100

    collect sf fix

--- Scripts/collect.py
+++ Scripts/collect.py
@@ -1,13 +1,15 @@
 import os
 import pandas as pd 
 
-if not os.path.isfile(snakemake.output[0]):
+sim = snakemake.params.simulation
+path = snakemake.params.resultpath
+
+if not os.path.isfile(path):
     df = pd.DataFrame({'sim' : [],'RMSE' : [], 'xcorlen' : [] , 'zcorlen' : [], 'frac' : []})
 else:
-    df = pd.read_csv(snakemake.input[0])
+    df = pd.read_csv(path)
 
 
-sim = snakemake.params.simulation
 
 x = sim['xcorlens']
 df['xcorlen'] = x
@@ -17,6 +19,6 @@ f = sim['fracs']
 df['frac'] = f
 
 
-Alldf = pd.read_csv(snakemake.output[0])
+Alldf = pd.read_csv(path)
 Alldf = pd.concat(Alldf, df)
-Alldf = pd.to_csv(snakemake.output[0])
+Alldf = pd.to_csv(path)
