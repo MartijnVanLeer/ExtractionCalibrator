f9dd7982716a47af35da16afa505017d29fa428d
MartijnVanLeer <100680324+MartijnVanLeer@users.noreply.github.com>
2024-03-25 15:24:24 +0100

    collector fix

--- Scripts/collect.py
+++ Scripts/collect.py
@@ -3,13 +3,15 @@ import pandas as pd
 
 if not os.path.isfile(snakemake.output[0]):
     df = pd.DataFrame({'sim' : [],'RMSE' : [], 'xcorlen' : [] , 'zcorlen' : [], 'frac' : []})
-    df.to_csv(snakemake.output[0])
+else:
+    df = pd.read_csv(snakemake.input[0])
+
 
 sim = snakemake.params.simulation
 
-df = pd.read_csv(snakemake.input[0])
-df['xcorlen'] = sim['xcorlen']
-df['zcorlen'] = sim['zcorlen']
+
+df['xcorlen'] = sim['xcorlens']
+df['zcorlen'] = sim['zcorlens']
 df['frac'] = sim['fracs']
 
 
