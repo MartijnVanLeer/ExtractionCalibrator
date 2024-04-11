a6263bad6184bf1c18bb0b9fa32231e75a30de2f
MartijnVanLeer <100680324+MartijnVanLeer@users.noreply.github.com>
2024-04-11 10:57:44 +0200

    debug sesh

--- Scripts/Upscaler.py
+++ Scripts/Upscaler.py
@@ -60,6 +60,7 @@ for cellid in tqdm(ids):
                                 Ly = k.shape[1] *real_dx,
                                 Lz = k.shape[2],
                                 dx = real_dx,dy = real_dx,dz = 1, mds = mds, ws = ws)
+                print(fieldK)
                 result.loc[dict(icell2d = cellid, sim = sim, cc = corfac)] = fieldK
             
 
