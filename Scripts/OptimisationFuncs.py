99d7b3c38797a0ed82a033a0f2299ce06bf94e86
MartijnVanLeer <100680324+MartijnVanLeer@users.noreply.github.com>
2024-04-26 11:44:05 +0200

    meer debug

--- Scripts/OptimisationFuncs.py
+++ Scripts/OptimisationFuncs.py
@@ -72,6 +72,8 @@ def GetObs(model_name, Location, idx,ds):
     ObsWells = pd.read_csv(os.path.join('..','Results',f'{model_name}',f'ObsForCalibration_{Location}.csv'))
     ObsHeads = pd.read_csv(os.path.join('..','Data','Preprocessed',f'stijghoogtereeksen_{Location}.csv'), index_col= 'Time')
     print(ObsHeads.shape)
+    print(idx)
+     sleep(100)
     ObsWells = ObsWells[ObsWells['Layno'].isin(idx.idx)]
     columns = list(set(ObsWells.putcode).intersection(ObsHeads.columns))
     ObsHeads = ObsHeads[columns]
