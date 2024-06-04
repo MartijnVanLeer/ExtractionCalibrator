1823de6f153381ddec0db16f987e915812f52601
MartijnVanLeer <100680324+MartijnVanLeer@users.noreply.github.com>
2024-06-04 14:43:08 +0200

    syntax

--- Scripts/MergeBest.py
+++ Scripts/MergeBest.py
@@ -20,7 +20,7 @@ print(realizations)
 for index, row in Best.iterrows():
     TempDS = xr.open_dataset(os.path.join('..', 'Results', modelname, 'KfieldsQC',f'xcorlens~{int(Best.xcorlen.values[0])}', f'zcorlens~{int(Best.zcorlen.values[0])}', f'fracs~{Best.frac.values[0]}', 'UpscaledK.nc'))
 realizations = realizations.expand_dims({'icell2d' : TempDS.icell2d.values})
-realizations['k'] = xr.DataArray(coords = (realizations.index, realizations.icell2d)
+realizations['k'] = xr.DataArray(coords = (realizations.index, realizations.icell2d))
 for index, row in Best.iterrows():
     TempDS = xr.open_dataset(os.path.join('..', 'Results', modelname, 'KfieldsQC',f'xcorlens~{int(row.xcorlen)}', f'zcorlens~{int(row.zcorlen)}', f'fracs~{row.frac}', 'UpscaledK.nc'))
     Vals = TempDS.sel(sim = row.sim, cc = row.cc)
