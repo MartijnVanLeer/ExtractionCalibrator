262f57a8d680f45b678de92d3f65271bb2021097
MartijnVanLeer <100680324+MartijnVanLeer@users.noreply.github.com>
2024-06-04 14:24:13 +0200

    MergeBest proper row

--- Scripts/MergeBest.py
+++ Scripts/MergeBest.py
@@ -16,12 +16,13 @@ Best = results[results.RMSE < RMSE]
 Best['k'] = np.nan
 
 realizations = xr.Dataset.from_dataframe(Best.reset_index())
+print(realizations)
 for index, row in Best.iterrows():
     TempDS = xr.open_dataset(os.path.join('..', 'Results', modelname, 'KfieldsQC',f'xcorlens~{int(row.xcorlen)}', f'zcorlens~{int(row.zcorlen)}', f'fracs~{row.frac}', 'UpscaledK.nc'))
     Vals = TempDS.sel(sim = row.sim, cc = row.cc)
     if 'icell2d' not in realizations.dims.values():
         realizations = realizations.expand_dims({'icell2d' : TempDS.icell2d.values})
-    realizations['k'].loc[index] = Vals.k
+    realizations.loc[index, :]['k'] = Vals.k.values
 
 realizations.to_netcdf(os.path.join('..', 'Results', modelname, 'BestRealizations.nc'))
 
