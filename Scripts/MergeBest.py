07c765c4a24f323a5587dc6d016991fb5ea18f35
MartijnVanLeer <100680324+MartijnVanLeer@users.noreply.github.com>
2024-06-04 14:29:36 +0200

    MergeBest cellid fix

--- Scripts/MergeBest.py
+++ Scripts/MergeBest.py
@@ -13,15 +13,17 @@ RMSE = np.sqrt(np.mean(residuals**2))
 Best = results[results.RMSE < RMSE]
 
 
-Best['k'] = np.nan
+
 
 realizations = xr.Dataset.from_dataframe(Best.reset_index())
 print(realizations)
+for index, row in Best.iterrows()[0]:
+    TempDS = xr.open_dataset(os.path.join('..', 'Results', modelname, 'KfieldsQC',f'xcorlens~{int(row.xcorlen)}', f'zcorlens~{int(row.zcorlen)}', f'fracs~{row.frac}', 'UpscaledK.nc'))
+realizations = realizations.expand_dims({'icell2d' : TempDS.icell2d.values})
+realizations['k'] = np.nan
 for index, row in Best.iterrows():
     TempDS = xr.open_dataset(os.path.join('..', 'Results', modelname, 'KfieldsQC',f'xcorlens~{int(row.xcorlen)}', f'zcorlens~{int(row.zcorlen)}', f'fracs~{row.frac}', 'UpscaledK.nc'))
     Vals = TempDS.sel(sim = row.sim, cc = row.cc)
-    if 'icell2d' not in realizations.dims.values():
-        realizations = realizations.expand_dims({'icell2d' : TempDS.icell2d.values})
     realizations.loc[index, :]['k'] = Vals.k.values
 
 realizations.to_netcdf(os.path.join('..', 'Results', modelname, 'BestRealizations.nc'))
