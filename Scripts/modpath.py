e441f23a36816e3599dd4ea49e115a2adf4dec40
MartijnVanLeer <100680324+MartijnVanLeer@users.noreply.github.com>
2024-06-05 23:09:33 +0200

    modpath welnodes fix

--- Scripts/modpath.py
+++ Scripts/modpath.py
@@ -3,9 +3,11 @@ import OptimisationFuncs
 import ModpathFuncs as mf
 import xarray as xr
 import pandas as pd
+import nlmod
 
 modelname = snakemake.params.modelname
 layer = snakemake.params.layer
+
 orgFolder = os.path.join('..','Results',f'{modelname}', f'{modelname}_ss', 'Fitter')
 destFolder = os.path.join('..','Results',f'{modelname}', f'{modelname}_ss', 'ModpathRuns')
 OptimisationFuncs.copyOriginalFolder(modelname + '_ss', orgFolder ,destFolder , 'Fitter\\' )
@@ -13,19 +15,22 @@ OptimisationFuncs.copyOriginalFolder(modelname + '_ss', orgFolder ,destFolder , 
 #Load model and ds
 ds = xr.open_dataset(os.path.join('..','Results',f'{modelname}', f'{modelname}_ss', f'{modelname}_ss.nc'))
 ds.attrs['model_ws'] = destFolder
-
 npfk, npfk33 = mf.load_calibrated_npf(modelname)
 sim, npf = mf.load_ss(destFolder,ds, npfk, npfk33)
 
+#get well nodes
+ExWells = pd.read_csv(os.path.join('..','Data','dawaco',f'winputten_WG_{Name}.csv'))
+wellxy = list(zip(ExWells['x_coordinaat'], ExWells['y_coordinaat']))
+
 #run ref model
-flowfrac_ref = mf.run_modpath_ref_bw(modelname, sim, ds, npf, layer)
-dist_ref = mf.run_modpath_ref_fw(modelname, sim, ds, npf, layer)
+flowfrac_ref = mf.run_modpath_ref_bw(modelname, sim, ds, npf, layer, wellxy)
+dist_ref = mf.run_modpath_ref_fw(modelname, sim, ds, npf, layer, wellxy)
 
 #load realizations
 rds = xr.open_dataset(os.path.join('..', 'Results', modelname, 'BestRealizations.nc'))
 
 #run modpath for realizations
-flowfrac, dist = mf.run_modpath_realizations(modelname,sim,ds,npf, rds, layer)
+flowfrac, dist = mf.run_modpath_realizations(modelname,sim,ds,npf, rds, layer, wellxy)
 flowfrac  = pd.DataFrame({'Flowfrac' : flowfrac, 'Realization' : 'Realizations'})
 flowfrac = pd.concat([flowfrac, pd.DataFrame({'Flowfrac' : flowfrac_ref, 'Realization' : 'Reference'}, index = [0])], ignore_index=True)
 flowfrac.to_csv(os.path.join('..','Results',f'{modelname}','flowfrac.csv'))
