3faf73c565cfa94c9caa83accb57eb07f6e90b44
MartijnVanLeer <100680324+MartijnVanLeer@users.noreply.github.com>
2024-10-10 14:20:03 +0200

    sim.write

--- Scripts/OptimisationFuncs.py
+++ Scripts/OptimisationFuncs.py
@@ -179,7 +179,7 @@ def run_calibration_ss(p, sim ,gwf, idx ,npf, npfk,npfk33, ghb,ghb_spd,ObsWells,
         npf.k33 = newk33
         # while True:
             # if os.access(os.path.join(sim.sim_path,npf.path[0] + '.' + npf.path[1]), os.W_OK):
-        npf.write()
+        # npf.write()
                 # break
     #ghb
 
@@ -202,10 +202,11 @@ def run_calibration_ss(p, sim ,gwf, idx ,npf, npfk,npfk33, ghb,ghb_spd,ObsWells,
         ghb = flopy.mf6.ModflowGwfghb(gwf, stress_period_data=new_spd,save_flows=False,maxbound = len(new_spd))
         # while True:
         #     if os.access(os.path.join(sim.sim_path,ghb.path[0] + '.' + ghb.path[1]), os.W_OK):
-        ghb.write()
+        # ghb.write()
                 # break
     
-    #run        
+    #run  
+    sim.write_simulation(silent = True)      
     sim.run_simulation(silent = True)
     
     #results
