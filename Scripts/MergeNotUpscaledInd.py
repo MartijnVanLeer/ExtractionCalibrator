4a4a99302735473c6a07416803663cd27dde91c6
MartijnVanLeer <100680324+MartijnVanLeer@users.noreply.github.com>
2025-01-30 11:38:08 +0100

    reset_index

--- Scripts/MergeNotUpscaledInd.py
+++ Scripts/MergeNotUpscaledInd.py
@@ -9,14 +9,14 @@ cal_results = pd.read_csv(os.path.join('..', 'Results', modelname, f'Calibration
 RMSE = cal_results['RMSE'].values[-1]
 KGE = cal_results['KGE'].values[-7]
 Best = results.loc[(results.RMSE < RMSE) | (results.KGE > KGE)]
-Best.reset_index(inplace = True, drop = True)
-Best.index.name = 'index'
+
 Best = Best[Best['xcorlen'].isin([700,800,1000,1200])]
 Best = Best[Best['zcorlen'] == 7.5]
 Best = Best[Best['frac'].isin([-0.05,0])]
 Best = Best[Best['cc'] == 1.5]
 Best = Best.sort_values('RMSE').head(3)
-
+Best.reset_index(inplace = True, drop = True)
+Best.index.name = 'index'
 print(Best.RMSE)
 realizations = xr.Dataset.from_dataframe(Best)
 row = Best.iloc[0]
